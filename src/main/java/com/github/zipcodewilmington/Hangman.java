474e6bd76fd20022c065d02d59303774bace4b5f
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-10-31 10:35:55 -0400

    started guessing game

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,5 +1,5 @@
 package com.github.zipcodewilmington;
-
+import java.util.Scanner;
 
 /**
  * @author xt0fer
@@ -7,4 +7,15 @@ package com.github.zipcodewilmington;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, contestant. would you like to start the game?");
+        System.out.println("Please enter \"yes\" or \"no\"");
+        String gameStart = scanner.nextLine();
+
+        while (gameStart == "yes"){
+            System.out.println("Let's begin");
+        }
+    }
 }
+
